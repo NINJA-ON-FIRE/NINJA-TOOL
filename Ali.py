28f7c1fffdc11f2b4e2d390eeecb9c3a84a50792
ALI RAJPOOT <155143384+NINJA-ON-FIRE@users.noreply.github.com>
2024-02-14 23:17:35 +0500

    Update Ali.py

--- Ali.py
+++ Ali.py
@@ -2,5 +2,6 @@ import os
 os.system("xdg-open https://chat.whatsapp.com/Dr8gnWcD7WdK6FKONbXyOu")
 try:import NINJAT
 except Exception as e:
-  if "32-bit" in str(e):
-    print("[×] 32Bit Not Supported ")
+  if "bit" in str(e):
+    import ninja_enc
+ #   print("[×] 32Bit Not Supported ")
